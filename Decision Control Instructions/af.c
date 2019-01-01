26d3fef54cd0088426919fabfbb3096f4e12865e
PramodJana <pramodjana395@gmail.com>
2019-01-01 21:47:49 +0530

    DCI

---
+++ Decision Control Instructions/af.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i=65;
+    char j= 'A';
+    if (i==j)
+        printf("C is WOW\n");
+    else
+        printf("C is headache\n");
+    return 0;
+}
+
+/*Answer
+
+C is WOW
+
+*/
